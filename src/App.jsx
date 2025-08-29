3f46e8878148cee51da9f32e4bb1bd1c42c9a940
sergii.ignatov <s.ignatov92@gmail.com>
2025-08-29 19:33:25 +0300

    styles changes and current weather is taken from API

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,42 @@
+import {useEffect, useState} from "react";
+
+const API_URL= import.meta.env.VITE_BASE_API_URL;
+const API_KEY = import.meta.env.VITE_API_KEY;
+
 const App = () => {
+
+    const [forecast, setForecast] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+    const [query, setQuery] = useState('Tallinn');
+
+    //fetch forecast
+    useEffect(() => {
+
+        const fetchForecast = async () =>{
+            try{
+                const response = await fetch(`${API_URL}${API_KEY}&q=${query}`);
+                const data = await response.json();
+                setForecast(data);
+            } catch (err) {
+                console.error(err.message);
+                setError(err.message);
+            } finally {
+                setLoading(false);
+            }
+        }
+        fetchForecast();
+    }, [query]);
+
+    if (loading) return <p>Loading forecast...</p>;
+    if (error) return <p>Error: {error}</p>;
+
     return (
         <>
             <header className="header">
-                <h1>Weather App</h1>
+                <h1>
+                    Weather App
+                </h1>
                 <div className="select-wrapper">
                     <select className="citySelect">
                         <option value="Tallinn">Tallinn</option>
@@ -14,28 +48,28 @@ const App = () => {
             <main className="main">
                 <div className="container">
                     <div className="summary">
-                        <h2 className="text-title">Tallinn, Harjumaa</h2>
+                        <h2 className="text-title">{forecast.location?.name?.toLocaleString()}, {forecast.location?.region?.toLocaleString()}</h2>
                         <p className="text-secondary text-m label">Current weather</p>
-                        <p className="text-xl m-0">⛅</p>
-                        <p className="text-l mb-0 mt-1">18°C</p>
-                        <p className="text-m text-secondary mt-1">Partly cloudy</p>
+                        <p className="text-xl m-0"><img src={`https:${forecast.current?.condition?.icon.toLocaleString()}`} alt="current weather icon"/></p>
+                        <p className="text-l mb-0 mt-1">{forecast.current?.temp_c.toLocaleString()} °C</p>
+                        <p className="text-m text-secondary mt-1">{forecast.current?.condition?.text}</p>
                     </div>
                     <div className="primary-details-grid">
                         <div className="detail-grid-item">
                             <label htmlFor="humidity" className="text-m text-secondary">Humidity</label>
-                            <div id="humidity" className="text-m">75%</div>
+                            <div id="humidity" className="text-m">{forecast.current?.humidity.toLocaleString()} %</div>
                         </div>
                         <div className="detail-grid-item">
                             <label htmlFor="wind-speed" className="text-m text-secondary">Wind speed</label>
-                            <div id="wind-speed"  className="text-m">20 km/h</div>
+                            <div id="wind-speed"  className="text-m">{forecast.current?.wind_kph.toLocaleString()} km/h ({forecast.current?.wind_dir.toLocaleString()})</div>
                         </div>
                         <div className="detail-grid-item">
                             <label htmlFor="high-temp" className="text-m text-secondary">High</label>
-                            <div id="high-tem"  className="text-m">20°C</div>
+                            <div id="high-tem"  className="text-m">{forecast.forecast.forecastday[0]?.day?.maxtemp_c.toLocaleString()} °C</div>
                         </div>
                         <div className="detail-grid-item">
                             <label htmlFor="low-temp" className="text-m text-secondary">Low</label>
-                            <div id="low-tem"  className="text-m">12°C</div>
+                            <div id="low-tem"  className="text-m">{forecast.forecast.forecastday[0]?.day?.mintemp_c.toLocaleString()} °C</div>
                         </div>
                     </div>
                 </div>
